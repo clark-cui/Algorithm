623c17e4a8db14051cdeb6194bdb70f203f9288b
clark-cui <cuirongchuan@163.com>
2020-08-16 22:21:51 +0800

    border

--- 分治DC.js
+++ 分治DC.js
@@ -1,10 +1,15 @@
 
 
-//递归求和
+//数组递归求和
 let a=(b)=>{
+    //边界判断
+    if(b.length===0){
+        return 0
+    }
+    //分治思想减少数据规模
     let c=b[0]
     b.shift();
-
+    //基线条件
     if(b.length===0){
         return c
     }
@@ -12,3 +17,4 @@ let a=(b)=>{
 }
 
 console.log (a([1,2,3]))
+console.log(a([]))
